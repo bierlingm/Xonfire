d46ec015e1a7b018576b581944f685a0171226fe
acuariano <acuariano@gmail.com>
2014-10-23 14:53:41 -0300

    Fixed bugs that failed the build

--- app/src/main/java/com/example/xonfire/xonfire/LogList.java
+++ app/src/main/java/com/example/xonfire/xonfire/LogList.java
@@ -28,7 +28,7 @@ public class LogList extends Activity {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-        if (id == R.id.action_settings) {
+        if (id == R.string.action_settings) {
             return true;
         }
         return super.onOptionsItemSelected(item);
