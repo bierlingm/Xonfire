c403ed180e655c79db170e868e391969ecbc0da2
EmanuelN <emanuelneves10@gmail.com>
2014-10-28 12:10:41 -0300

    Added imagebuttons to LogTask activity

--- app/src/main/java/com/example/xonfire/xonfire/LogTask.java
+++ app/src/main/java/com/example/xonfire/xonfire/LogTask.java
@@ -4,6 +4,8 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageButton;
 
 
 public class LogTask extends Activity {
@@ -12,6 +14,26 @@ public class LogTask extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_task);
+
+        //Declaring all imagebuttons
+        final ImageButton spiritBtn = (ImageButton) findViewById(R.id.imageButton);
+        final ImageButton workBtn = (ImageButton) findViewById(R.id.imageButton2);
+        final ImageButton heartBtn = (ImageButton) findViewById(R.id.imageButton3);
+
+        //Spirit Button option
+        View.OnClickListener spiritListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                /*The button was clicked, so remove borders to other buttons, add a border to this one,
+                change the variable to be equal to 0.
+                 */
+                workBtn.setBackgroundResource(R.drawable.noborder);
+                heartBtn.setBackgroundResource(R.drawable.noborder);
+                spiritBtn.setBackgroundResource(R.drawable.border);
+
+            }
+        };
+        spiritBtn.setOnClickListener(spiritListener);
     }
 
 
